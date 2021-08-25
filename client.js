74d4e4c19c9bf81c8915a563078b0b50df312937
isford <78660416+isford@users.noreply.github.com>
2021-08-25 10:48:57 -0500

    Jquery properly appending values

---
+++ client.js
@@ -0,0 +1,34 @@
+console.log('JS Loaded');//JS Test
+
+$(document).ready(readyNow);//Loading JQ
+
+let animalArray = [];//Animal cost added to array to find total
+
+function submitAnimal(){
+    console.log('Submit Clicked')
+    let animal = {
+        name: $('#nameIn').val(),
+        species: $('#speciesIn').val(),
+        age: $('#ageIn').val(),
+        surplus: $('#surplusIn').val(),
+        cost: $('#costIn').val(),
+        photo: $('#photoIn').val(),
+    }
+    console.log('The animal being added is', animal)
+
+    $('.table').append(`
+<tr class = "newRow">
+                <td id = "appLn">${animal.name}</td>
+                <td id = "appId">${animal.species}</td>
+                <td id = "appTitle">${animal.age}</td>
+                <td id = "appSalary">${animal.surplus}</td>
+                <td id = "appSalary">${animal.cost}</td>
+                <td id = "appSalary">${animal.photo}</td>
+                <td><button class="btn btn-danger" id = "deleteBtn" >Delete</button></td>
+</tr>`)
+}
+
+function readyNow(){
+    console.log('JQ Loaded');
+    $('#submit').on('click', submitAnimal)
+}
